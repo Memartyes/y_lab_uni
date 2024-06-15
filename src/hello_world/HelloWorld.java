9388f2431599e7446a48f4fa38814424e9cbd557
Memartyes <36759647+Memartyes@users.noreply.github.com>
2024-06-15 14:54:29 +0300

    just added comments to the first project method main to check that the 'Commit and Push...' works

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -2,6 +2,7 @@ package hello_world;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        //the first project program start
         System.out.println("Hello World!");
     }
 }
